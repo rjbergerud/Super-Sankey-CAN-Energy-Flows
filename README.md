39696eb9625439103846b719356d395f105d9865
Ryder Bergerud <ryder.bergerud@gmail.com>
2020-12-03 20:27:53 -0800

    Initial setup

---
+++ README.md
@@ -0,0 +1,22 @@
+# Canadian Energy Flows Super Sankey
+
+Inspired by the work of Otherlabs who created a detailed sankey diagram of [US energy flows](http://departmentof.energy/).
+
+## Setup
+
+To setup your environment, either run
+```
+conda env create --file environment.yaml
+```
+or install project requirements with pip
+```
+pip install -r requirements.txt
+```
+
+The required datasets can be downloaded by running the script from the root directory of the project
+```
+cd data
+bash download_data.sh
+```
+
+Any required datasets should be added to this list.
---
+++ data/download_data.sh
@@ -0,0 +1,9 @@
+# Layer 1
+wget https://www.cer-rec.gc.ca/open/imports-exports/electricity-exports-and-imports-annual.csv
+wget https://www.cer-rec.gc.ca/open/imports-exports/electricity-exports-and-imports-data-dictionary.csv
+wget https://www.cer-rec.gc.ca/open/imports-exports/data/imports-exports-data-visualization.csv
+wget https://www.cer-rec.gc.ca/open/imports-exports/natural-gas-exports-and-imports-annual.csv
+wget https://www.cer-rec.gc.ca/open/imports-exports/natural-gas-exports-and-imports-data-dictionary.csv
+
+# Layer 2
+wget https://www150.statcan.gc.ca/n1/tbl/csv/25100029-fra.zip
