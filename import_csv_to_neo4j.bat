84610afc03e5ad45685c630c0622ffb1d42661cb
Ryder Bergerud <ryder.bergerud@gmail.com>
2020-12-15 22:52:43 -0800

    Need to specify level for visualization

--- import_csv_to_neo4j.bat
+++ import_csv_to_neo4j.bat
@@ -2,16 +2,18 @@
 //skip null values
 LOAD CSV WITH HEADERS FROM 'file:///energy_sankey_current.csv' AS row
 WITH row WHERE row.value IS NOT NULL
-MERGE (c:Category {name: row.source});
+MERGE (c:Category {name: row.source, level: toInteger(row.level)});
 
 // Add all rh nodes
 LOAD CSV WITH HEADERS FROM 'file:///energy_sankey_current.csv' AS row
 WITH row WHERE row.value IS NOT NULL
-MERGE (c:Category {name: row.target});
+MERGE (c:Category {name: row.target, level: toInteger(row.level) - toInteger(1)});
 
 // Add edges
 LOAD CSV WITH HEADERS FROM 'file:///energy_sankey_current.csv' AS row
 WITH row WHERE row.value IS NOT NULL
 MATCH (s:Category {name: row.source})
 MATCH (t:Category {name: row.target})
-CREATE (s)-[:FLOW {value: row.value}]->(t);
+CREATE (s)-[:FEEDS {value: row.value}]->(t);
+
+// MATCH o=(n)-[:FEEDS]->(d) WHERE n.level is not null and d.level is not null RETURN n,o,d ORDER BY n.level DESC
